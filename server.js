d32a0e660c3e1265d88788b763e76239705a341f
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-14 19:33:58 -0400

    connecting to server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Spender";
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+   useFindAndModify: false
 });
 
 // routes
@@ -27,3 +27,6 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+
+//https://maddi124.github.io/Spender/
