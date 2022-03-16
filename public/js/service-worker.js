9fe41efebac28a43c906a1573d7f2877beb0ff8e
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-15 22:43:03 -0400

    fixed serviceworker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -8,10 +8,15 @@ const FILES_TO_CACHE = [
 "./index.js",
 "./style.css",
 "./manifest.json",
-".js/icons/icon-192x192.png"
-
+"icons/icon-72x72.png",
+"icons/icon-96x96.png",
+"icons/icon-128x128.png",
+"icons/icon-144x144.png",
+"icons/icon-152x152.png",
+"icons/icon-192x192.png",
+"icons/icon-384x384.png",
+"icons/icon-512x512.png",
 ];
-
 
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
